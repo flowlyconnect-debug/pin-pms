6a891f3b395d776f6160c15931246b8d089d8efe
Matias <matias.flowly@gmail.com>
2026-04-29 23:14:27 +0300

    fix deploy import error

--- app/backups/__init__.py
+++ app/backups/__init__.py
@@ -1,7 +1,4 @@
 from flask import Blueprint
 
 backups_bp = Blueprint("backups", __name__)
-backups_admin_bp = Blueprint("backups_admin", __name__)
-
-# Attach backup admin routes after blueprint creation.
-from . import routes  # noqa: E402,F401
+from .routes import backups_admin_bp
